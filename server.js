0867c8e6d0525e6289a31d45a1017c9a5805288c
faizubg <faizubg@gmail.com>
2026-04-12 15:09:51 +0100

    Add dependencies and bootcamps controller
    Add .gitignore and controllers/bootcamps.js and bulk-add project dependencies (node_modules) to set up required packages for the application.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Mount routers
+const bootcamps = require('./routes/bootcamps');
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
