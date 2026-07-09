24bc24b08a446ed4e58197bb5ba6c6c1f06abad0
faizubg <faizubg@gmail.com>
2026-07-09 15:30:47 +0100

    Enhance bootcamps/courses controllers & add seed data
    Add users and reviews seed data and update bootcamps/courses fixtures. Enhance Bootcamp controller with filtering, select, sort, pagination, course population and a geolocation radius endpoint; change delete flow to trigger model removal. Add course controller actions (get single course, add course) and populate bootcamp info. Update Bootcamp model to enable virtuals, add a virtual 'courses' relationship and cascade-delete courses on bootcamp removal. Wire new course routes for GET/:id and POST (nested).

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Courses = require('../models/Course'); 
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc Get courses
 // @route GET /api/v1/courses
@@ -12,7 +13,10 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampId) {
     query = Courses.find({ bootcamp: req.params.bootcampId });
   } else {
-    query = Courses.find();
+    query = Courses.find().populate({
+      path: 'bootcamp',
+      select: 'name description'
+    });
   }
 
     const courses = await query;
@@ -21,5 +25,46 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
         success: true,
         count: courses.length,
         data: courses
+    });
+});
+
+// @desc Get single course
+// @route GET /api/v1/courses/:id
+// @access Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+    const course = await Courses.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    });
+
+    if (!course) {
+        return next(new ErrorResponse(`Course not found with id of ${req.params.id}`, 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
+
+// @desc Add course
+// @route POST /api/v1/bootcamps/:bootcampId/courses
+// @access Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
+    req.body.user = req.user.id;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if (!bootcamp) { 
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.bootcampId}`, 404));
+    }
+
+    const course = await Courses.create(req.body);
+
+
+    res.status(201).json({
+        success: true,
+        data: course
     });
 });
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -85,7 +85,11 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
+
 // Create bootcamp slug from the name
 BootcampSchema.pre('save', function() {
     this.slug = slugify(this.name, { lower: true });
@@ -109,8 +113,20 @@ BootcampSchema.pre('save', async function() {
     };
     this.address = undefined;
 });
-// Create bootcamp slug from the name
-    // Do not save address in DB
-    this.address = undefined;
+
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function(next) {
+    console.log(`Courses being removed from bootcamp ${this._id}`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+});
+
+// Reverse populate with virtuals
+BootcampSchema.virtual('courses', {
+    ref: 'Course',
+    localField: '_id',
+    foreignField: 'bootcamp',
+    justOne: false
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
